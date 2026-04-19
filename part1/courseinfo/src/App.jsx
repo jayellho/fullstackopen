fb17586fb008fb06b9c62c794adf9d1c15410c81
jayellho <jayellho@gmail.com>
2026-04-18 22:24:07 -0700

    refactor: components should not be within components; also accidentally finished exercise 1.9.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,33 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  ) 
+}
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  )
+}
+
 const App = () => {
   
   const course = {
@@ -16,36 +46,6 @@ const App = () => {
         exercises: 14
       }
     ]
-  }
-  
-  const Header = ({ course }) => {
-    return <h1>{course.name}</h1>
-  }
-
-  const Part = ({ part, exercises }) => {
-    return (
-      <p>
-        {part} {exercises}
-      </p>
-    )
-  }
-
-  const Content = ({ parts }) => {
-    return (
-      <div>
-        <Part part={parts[0].name} exercises={parts[0].exercises} />
-        <Part part={parts[1].name} exercises={parts[1].exercises} />
-        <Part part={parts[2].name} exercises={parts[2].exercises} />
-      </div>
-    ) 
-  }
-
-  const Total = ({ parts }) => {
-    return (
-      <p>
-        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      </p>
-    )
   }
 
   return (
