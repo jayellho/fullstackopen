cb84b87e548d2700972811c0e50d14110d2d595d
jayellho <jayellho@gmail.com>
2026-04-21 22:12:55 -0700

    feat: complete exercise 2.1.

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,16 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <ul>
+        {course.parts.map(part => (
+          <li key={part.id}>
+            {part.name} {part.exercises}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Course
